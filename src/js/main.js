b9dcfeaf591ff6550069f3c070fc3f5cd6d71004
Korchin Maxim <98269691+korchinmv@users.noreply.github.com>
2022-06-21 11:19:38 +0300

    Delete src directory

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import "./functions/burger";
-import "./functions/header-fixed";
-import "./functions/map";
-import "./functions/sliders";
-import "./functions/smooth-scroll";
-// import "./plugins/load-more";
-// import "./plugins/map";
-// import "./plugins/mask";
